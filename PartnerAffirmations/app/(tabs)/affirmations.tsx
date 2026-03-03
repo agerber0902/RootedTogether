2285093e65240a1c45101d80a6262fdfe5f97e46
agerber0902 <agerber0902@gmail.com>
2026-03-02 20:57:51 -0500

    display user creatored affirmations as affirmationtext on affirmation screen

--- PartnerAffirmations/app/(tabs)/affirmations.tsx
+++ PartnerAffirmations/app/(tabs)/affirmations.tsx
@@ -1,11 +1,43 @@
 import AffirmationHeader from "@/components/affirmations/affirmation-header";
+import AffirmationText from "@/components/affirmations/affirmation-text";
+import SharedCard from "@/components/shared/shared-card";
 import SharedSafeView from "@/components/shared/shared-safe-view";
+import SharedText from "@/components/shared/shared-text";
+import { Affirmation } from "@/constants/models/affirmation";
+import { getUserCreatedAffirmations } from "@/helpers/affirmation-helper";
+import { useAuth } from "@/providers/auth-provider";
+import { useAppDispatch, useAppSelector } from "@/state/hooks";
+import { setUserCreatedAffirmations } from "@/state/slices/affirmation";
+import { useEffect } from "react";
+import { ScrollView, Text } from "react-native";
 
 const AffirmationsScreen = () => {
+    const { user } = useAuth();
+    
+    const dispatch = useAppDispatch();
+    const { userCreatedAffirmations } = useAppSelector((state) => state.affirmation.value);
+
+    useEffect(() => {
+        const getDisplayAffirmations = async() => {
+            const createdAffirmations = await getUserCreatedAffirmations(user?.uid ?? '0');
+
+            dispatch(setUserCreatedAffirmations(createdAffirmations));
+        }
+
+        getDisplayAffirmations();
+    }, [dispatch, user]);
 
     return <>
         <SharedSafeView header={<AffirmationHeader/>}>
-            <></>
+            <>
+                <SharedCard visible={true}>
+                    <ScrollView scrollEnabled={true}>
+                       {userCreatedAffirmations.map((affirmation: Affirmation) => (
+                            <AffirmationText key={affirmation.id} text={affirmation.message}/>
+                       ))}
+                    </ScrollView>
+                </SharedCard>
+            </>
         </SharedSafeView>
     </>;
 };
--- PartnerAffirmations/components/affirmations/affirmation-text.tsx
+++ PartnerAffirmations/components/affirmations/affirmation-text.tsx
@@ -1,18 +1,18 @@
 import FadeInView from "../shared/fade-in-animated-view";
 import { affirmationTextStyles } from "@/constants/stylesheets/components/affimations/affirmation-text";
-import { useAppSelector } from "@/state/hooks";
 import SharedText from "../shared/shared-text";
 
-const AffirmationText = () => {
-  const style = affirmationTextStyles;
+type AffirmationTextProps = {
+  text: string;
+};
 
-  const { todaysAffirmation } = useAppSelector((state) => state.affirmation.value);
-  const message = todaysAffirmation?.affirmation?.message ?? 'You are the designer of your best life';
+const AffirmationText = ({text} : AffirmationTextProps) => {
+  const style = affirmationTextStyles;
 
   return (
     <>
       <FadeInView style={style.textContainer}>
-        <SharedText numberOfLines={3} style={style.textContent} text={message} />
+        <SharedText numberOfLines={3} style={style.textContent} text={text} />
       </FadeInView>
     </>
   );
