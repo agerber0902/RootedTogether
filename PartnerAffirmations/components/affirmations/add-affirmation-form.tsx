8f513b7363250d6a9826162eb285a409afd48d9d
agerber0902 <agerber0902@gmail.com>
2026-03-04 21:05:31 -0500

    order by created at date, then display date

--- PartnerAffirmations/components/affirmations/add-affirmation-form.tsx
+++ PartnerAffirmations/components/affirmations/add-affirmation-form.tsx
@@ -5,9 +5,11 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { KeyboardAvoidingView, Platform, TextInput, View } from "react-native";
 import Button from "../shared/button";
 import LoadingSpinner from "../shared/loading-spinner";
-import { addAffirmation } from "@/helpers/affirmation-helper";
+import { addAffirmation, getUserCreatedAffirmations } from "@/helpers/affirmation-helper";
 import { useAuth } from "@/providers/auth-provider";
 import SharedTextInput from "../shared/shared-text-input";
+import { useAppDispatch } from "@/state/hooks";
+import { setUserCreatedAffirmations } from "@/state/slices/affirmation";
 
 type AddAffirmationFormProps = {
   isLoading: boolean;
@@ -23,6 +25,8 @@ const AddAffirmationForm = ({
 
   const [message, setMessage] = useState<string | undefined>(undefined);
 
+  const dispatch = useAppDispatch();
+
   const handleAdd = async () => {
     //TODO: Validate input
     if (message === undefined) {
@@ -33,8 +37,12 @@ const AddAffirmationForm = ({
       setIsLoading(true);
 
       // Add to data base
-      addAffirmation({message, recipientId: user!.uid, creatorId: user!.uid});
-      setMessage(undefined); // reset input
+      await addAffirmation({message, displayDate: null, recipientId: user!.uid, creatorId: user!.uid});
+
+      // Refetch the list
+      dispatch(setUserCreatedAffirmations(await getUserCreatedAffirmations(user!.uid)));
+
+      setMessage(''); // reset input
     } catch (error) {
       console.error("Failed to add affirmation:", error);
     } finally {
