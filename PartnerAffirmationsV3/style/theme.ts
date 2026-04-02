821eca9935e19368551b03d56aa37a3930a3ab3d
agerber0902 <agerber0902@gmail.com>
2026-04-01 20:28:44 -0400

    create base theme object to be used across styles

---
+++ PartnerAffirmationsV3/style/theme.ts
@@ -0,0 +1,14 @@
+// Main Theme Object
+// Carries values and not style objects
+export const Theme = {
+  // App Color Scheme
+  colorScheme: {
+    background: "#F6F0EA",
+    tabBackground: "#F6F0EA",
+
+    card: "#FFFCF9",
+    modal: "#F6F0EA",
+  },
+  //   App Fonts
+  fonts: {},
+};
